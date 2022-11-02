e18ca57e78abd7141b9a16faf4528d128b7d44c3
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-11-02 15:12:26 +0530

    css

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 import "./App.css";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [listOfPost, setListOfPost] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:3001/posts").then((responce) => {
-      console.log(responce.data);
+      setListOfPost(responce.data);
     });
   }, []);
+  return (
+    <div className="App">
+      {listOfPost.map((value, key) => {
+        return (
+          <div className="post">
+            <div className="title">{value.title}</div>
+            <div className="body">{value.postText}</div>
+            <div className="footer">{value.username}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
