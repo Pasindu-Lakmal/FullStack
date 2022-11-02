a4bca96cb1b58cc06ee7f5e1b6a13db8cf28a4b7
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-11-02 13:07:05 +0530

    mui

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
 import "./App.css";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Abc from "./components/Abc";
+import Post from "./components/Post";
+import { Card } from "@mui/material";
 
 function App() {
+  const [listOfPosts, setListOfPosts] = useState([]);
+
   useEffect(() => {
     axios.get("http://localhost:3001/posts").then((responce) => {
       console.log(responce.data);
+      setListOfPosts(responce.data);
     });
   }, []);
+
+  return (
+    <div>
+      <div className="App">
+        {listOfPosts.map((value, key) => {
+          return (
+            <Post
+              title={value.title}
+              postText={value.postText}
+              userName={value.username}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
