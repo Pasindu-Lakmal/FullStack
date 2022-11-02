9447e0ae9943b5f5340b5e7bab9a3c55d1a077d1
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-11-02 08:17:27 +0530

    useAxios

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const db = require("./models");
+const cors = require("cors");
 
 app.use(express.json());
-
+app.use(cors());
 //Routers
 const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
